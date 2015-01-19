47bd020c10bd82d1fde917d601225c36bd42b889
Ryan <ryanpauley@gmail.com>
2015-01-19 16:11:44 +1100

    Index and show created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+ # create_table "posts", force: :cascade do |t|
+ #    t.string   "title"
+ #    t.text     "content"
+ #    t.string   "tag"
+ #    t.datetime "created_at", null: false
+ #    t.datetime "updated_at", null: false
+ #  end
+
+ firstpost = Post.create({
+ 	title: "Hello World",
+ 	content: "This is the first post (It will be used to test the seedfile).",
+ 	tag: "meta, code, rubyonrails, generalassembly"
+ 	})
